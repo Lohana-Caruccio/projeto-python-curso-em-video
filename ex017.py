5b3aadef8ef38d4cdee35b536a9870ca09dc3ff4
Lohana-Caruccio <lohana472@gmail.com>
2026-03-19 11:48:36 -0300

    Novos exercícios

--- ex017.py
+++ ex017.py
@@ -4,4 +4,4 @@ opos = float(input('Digite o valor do cateto oposto: '))
 quadradohi = pow(adj,2) + pow(opos,2)
 hipotenusa = sqrt(quadradohi)
 print('Dado o valor do cateto adjacente {:.2f} e do cateto oposto de {:.2f}.\n'
-      'O comprimento da sua hipotenusa será {:.2f}'.format(adj, opos, hipotenusa))
+      'O comprimento da sua hipotenusa será {:.2f}.'.format(adj, opos, hipotenusa))
