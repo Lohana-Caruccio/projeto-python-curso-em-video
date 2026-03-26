704abe9405f65038f3e387f09138aa8466c4edc3
Lohana-Caruccio <lohana472@gmail.com>
2026-03-26 17:19:06 -0300

    Exercício novo

--- ex026.py
+++ ex026.py
@@ -1,4 +1,4 @@
 frase = str(input('Digite uma frase qualquer: ')).strip().upper()
 print('A letra "A" aparece {} vezes na frase'.format(frase.count('A')))
-print('A letra "A" aparece primeiro na posição {}'.format(frase.find('A')))
-print('A letra "A" aparece pela última vez na posição {}'.format(frase.rfind('A')-frase.count(' ')))
+print('A letra "A" aparece primeiro na posição {}'.format(frase.find('A')+1))
+print('A letra "A" aparece pela última vez na posição {}'.format(frase.rfind('A')-frase.count(' ')+1))
