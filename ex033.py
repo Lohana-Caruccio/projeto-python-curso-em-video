2374c065d6c7df6449010fa33ebc9e4775aba749
Lohana-Caruccio <lohana472@gmail.com>
2026-03-28 17:38:55 -0300

    Pequenas mudanças

--- ex033.py
+++ ex033.py
@@ -1,21 +1,17 @@
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo numero: '))
 num3 = int(input('Digite o terceiro numero: '))
-if num1 > num2 and num1 > num3:
-    print('O {} é o maior número'.format(num1))
-    if num2 > num3:
-        print('O {} é o menor número'.format(num3))
-    else:
-            print('O {} é o menor número'.format(num2))
-if num1 < num2 and num1 < num3:
-    print('O {} é o menor número'.format(num1))
-    if num2 < num3:
-        print('O {} é o maior número'.format(num3))
-    else:
-        print('O {} é o maior número'.format(num2))
-else:
-    if num2 > num3:
-        print('O {} é o maior número e o {} é o menor'.format(num2, num3))
-    else:
-        print('O {} é o maior número e o {} é o nenor'.format(num3,num2))
-
+#Verificando quem é o menor
+menor = num1
+if num2 < num1 and num2 < num3:
+    menor = num2
+if num3 < num1 and num3 < num2:
+    menor = num3
+#Verificando quem é o maior
+maior = num1
+if num2 > num1 and num2 > num3:
+    maior = num2
+if num3 > num1 and num3 > num2:
+    maior = num3
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
