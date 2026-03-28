5e55cfd5eefb2d4c5e9956647db82b08a24e2877
Lohana-Caruccio <lohana472@gmail.com>
2026-03-28 18:04:55 -0300

    Pequenas mudanças

--- ex035.py
+++ ex035.py
@@ -1,7 +1,12 @@
+print('-=-'*9)
+print('Analisando um triângulo...')
+print('-=-'*9)
 comp1 = float(input('Digite o comprimento da primeira reta: '))
 comp2 = float(input('Digite o comprimento da segunda reta: '))
 comp3 = float(input('Digite o comprimento da terceira reta: '))
 if comp1 + comp2 > comp3:
-    print('Da para formar um triângulo')
+    print(' ')
+    print('Esses segmentos PODEM formar um triângulo')
 else:
-    print('Não dá para formar um triângulo')
+    print(' ')
+    print('Esses segmentos NÃO PODEM formar um triângulo')
