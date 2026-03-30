17b6dc1462231b335fdc25cc442c0a1b0c6e8e6a
Lohana-Caruccio <lohana472@gmail.com>
2026-03-30 16:48:47 -0300

    Exercício novo

--- ex036.py
+++ ex036.py
@@ -7,6 +7,6 @@ salario = float(input('Digite o valor do seu  salário atual R$ '))
 anos = int(input('Em quantos anos deseja pagar: '))
 prestacao = valor / (anos * 12)
 if prestacao > salario * 30 / 100:
-    print('\033[1;31mEmpréstimo negado\033[m, a parcela passará de 30% do seu salário!')
+    print('\033[1;31mEmpréstimo negado\033[m, a parcela passará de 30% do seu salário.')
 else:
     print('\033[1;36mEmpréstimo aprovado!\033[m\nO valor da parcela ficará R${:.2f} por mês pelos próximos {} anos!'.format(prestacao,anos))
