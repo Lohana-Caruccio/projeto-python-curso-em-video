fb6e14748eb9046c065e22385b33320d665faee9
Lohana-Caruccio <lohana472@gmail.com>
2026-03-30 18:48:47 -0300

    Exercício novo

--- ex040.py
+++ ex040.py
@@ -5,8 +5,8 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 media = (nota1 + nota2) / 2
 if media >= 7:
-    print('Aprovado! Sua média foi {:.1f}'.format(media))
+    print('Você foi APROVADO! Sua média foi {:.1f}'.format(media))
 elif media < 5:
-    print('Reprovado! Sua média foi de {:.1f} e o mínimo era {}'.format(media,7.0))
+    print('Você foi REPROVADO! Sua média foi de {:.1f} e o mínimo era {}'.format(media,7.0))
 else:
-    print('Recuperação! Sua média foi {:.1f} e o mínimo era {}'.format(media,7.0))
+    print('Você pegou RECUPERAÇÃO! Sua média foi {:.1f} e o mínimo era {}'.format(media,7.0))
