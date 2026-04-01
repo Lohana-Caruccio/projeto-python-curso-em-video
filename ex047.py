80b751c36087f7d22b63334c76c52031d51b276d
Lohana-Caruccio <lohana472@gmail.com>
2026-04-01 11:12:41 -0300

    Exercício novo

--- ex047.py
+++ ex047.py
@@ -1,1 +1,7 @@
 #Números pares entre um intervalo
+from time import sleep
+apresentacao = str(input('Entre o intervalo de 1 a 50\nDigite enter para ver os números pares '))
+print(' ')
+for n in range(2,51,2):
+            print(n, end=' ')
+print('FIM')
