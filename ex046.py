c079b4780f41407b3997d75cb7faf33d6eb27815
Lohana-Caruccio <lohana472@gmail.com>
2026-04-01 10:36:50 -0300

    Exercício novo

---
+++ ex046.py
@@ -0,0 +1,12 @@
+#Contagem Regressiva para fogos
+from time import sleep
+print('CONTAGEM REGRESSIVA PARA O ANO NOVO COMEÇA AGORA!')
+print(' ')
+for c in range(10,-1,-1):
+  print(c)
+  sleep(0.7)
+print(' ')
+print('FELIZ ANO NOVO!!!')
+
+
+
