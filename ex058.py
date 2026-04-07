c6d95ec4cfe7f7bd06bcae78572ebc059e73bc66
Lohana-Caruccio <lohana472@gmail.com>
2026-04-07 15:53:40 -0300

    Exercício novo

---
+++ ex058.py
@@ -0,0 +1,21 @@
+#Jogo da advinhação
+from random import randint
+from time import sleep
+computador = randint(0,10)
+print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
+print(' ')
+cont = 0
+acertou = False
+while not acertou:
+    jogador = int(input('Em que número pensei? '))
+    cont += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador > computador:
+            print('Menos, tente novamente: ')
+        elif jogador < computador:
+            print('Mais, tente novamente: ')
+print('...')
+sleep(2)
+print('ACERTOU!\nVocê precisou de {} tentativas.'.format( cont))
