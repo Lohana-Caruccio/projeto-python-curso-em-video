2e41a3a0024a77167e5c6b745aeac2aec7b344e3
Lohana-Caruccio <lohana472@gmail.com>
2026-04-08 19:26:33 -0300

    Adicionei novos comentários

--- ex060.py
+++ ex060.py
@@ -1,12 +1,11 @@
-#Cálculo de fatorial
+#Cálculo de fatorial com alguns comentários para melhor entendimento
 n = int(input('Digite um número para calcular seu fatorial: '))
-cont = n
-fatorial = 1
-print('Calculando {}! ='.format(n), end=' ')
+cont = n #recebe n
+fatorial = 1 #recebe 1 para depois utilizá-lo na multiplicação
+print('Calculando {}! = '.format(n), end='')
 while cont > 0:
-    print('{} '.format(cont), end='')
-    print('x ' if cont > 1 else '=', end='')
-    fatorial = fatorial * cont
-    cont = cont - 1
-print(' {}'.format(fatorial))
-
+    print('{}'.format(cont), end='')
+    print(' x ' if cont > 1 else ' = ', end='') #vai printar o x até o último número da multiplicação ser 1, depois disso vai printar =
+    fatorial = fatorial * cont  #acumula desde de 1(que era seu valor inicial) x 5(ex de contador inicial) e vai acumulando, (5(novo valor)) x 5 - 1 x (4(acumula o novo valor)) x 4 - 1 ... até o final, a cada laço
+    cont = cont - 1  # diminui 1 do número enquanto o cont for maior que 0 e atua em conjunto do fatorial
+print('{}'.format(fatorial)) #resultado acumulado
