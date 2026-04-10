0d8aafe086c05b1f437ae8a170ff791c3264d9da
Lohana-Caruccio <lohana472@gmail.com>
2026-04-10 16:59:54 -0300

    Exercício novo

---
+++ ex066.py
@@ -0,0 +1,10 @@
+#Vários números com flag
+n = s = 0
+while True:
+    n = int(input('Digite um número [999 para parar]: '))
+    if n == 999:
+        break
+    s += n
+print('-=-'*15)
+print(f'A soma de todos os números digitados foi {s}!')
+
