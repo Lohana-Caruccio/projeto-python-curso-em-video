eb3ad4ebce0ad8b30faf7d9febff8e7b240cf173
Lohana-Caruccio <lohana472@gmail.com>
2026-04-16 10:56:08 -0300

    Exercício novo

--- ex072.py
+++ ex072.py
@@ -1,7 +1,15 @@
 #Número por extenso
 extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze',
          'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
-num = int(input('Digite um número de 0 a 20: '))
-while num not in range(0,21):
-    num = int(input('Tente novamente. Digite um número de 0 a 10: '))
-print(f'Você digitou o número {extenso[num]}!')
+while True:
+    num = int(input('Digite um número de 0 a 20: '))
+    while num not in range(0,21):
+        num = int(input('Tente novamente. Digite um número de 0 a 10: '))
+    print(f'Você digitou o número {extenso[num]}!')
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if continuar in 'N':
+         break
+print('--'*15)
+print('Fim do programa!')
