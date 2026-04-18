680adcdf1f526bda965045c87391c45a21d88f76
Lohana-Caruccio <lohana472@gmail.com>
2026-04-18 17:25:13 -0300

    Exercício novo

---
+++ ex076.py
@@ -0,0 +1,16 @@
+#Lista de Preços com Tupla
+listagem = ('Lápis', 1.75,
+            'Borracha', 2,
+            'Caderno', 15.90,
+            'Estojo', 25,
+            'Transferidor', 4.20,)
+print('-'*50)
+print('{:^48}'.format('LISTAGEM'' ''DE'' ''PREÇOS'))
+print('-'*50)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-'*50)
+
