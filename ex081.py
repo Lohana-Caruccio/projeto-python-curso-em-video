089f9b285ddf50584a75593c654833072be094d6
Lohana-Caruccio <lohana472@gmail.com>
2026-04-22 20:56:20 -0300

    Exercício novo

---
+++ ex081.py
@@ -0,0 +1,27 @@
+#Extraindo dados de uma Lista
+num = list()
+c = c5 = rE = 0
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in num:
+        num.append(n)
+        c += 1
+    else:
+        print('Valor repetido, tente outro...')
+    if n == 5:
+        c5 += 1
+    r = str(input('Quer continuar? [S/N] '))
+    while r not in 'SsNn':
+        r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+print('-='*30)
+print(f'Foram digitados, sem contar os repetidos, {c} valores em ordem decrescente:')
+print(f'\n{sorted(num, reverse=True)}')
+print(' ')
+if c5 == 0:
+    print('O valor 5 não foi digitado e não está na lista!')
+else:
+    print('O valor 5 foi digitado e está na lista!')
+
+
