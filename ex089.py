3181b66bc88eb6f8414161a8c9005a37d25cc962
Lohana-Caruccio <lohana472@gmail.com>
2026-04-28 19:39:05 -0300

    Exercício novo

---
+++ ex089.py
@@ -0,0 +1,52 @@
+#Boletim com listas compostas
+geral = list()
+media = 0
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    geral.append([nome, [nota1, nota2], media])
+    r = ' '
+    while r not in 'SsNn':
+        r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if r in 'Nn':
+        break
+print('-='*20)
+print(f'{"No.":<5}{"NOME":<10}{"MEDIA":>10}')
+print('--'*20)
+for i, a in enumerate(geral):
+    print(f'{i:<4}', f'{a[0]:<10}', f'{a[2]:>8.1f}')
+print('-' * 50)
+while True:
+    N = int(input('Mostrar notas de qual aluno?(999 interrompe): '))
+    print(' ')
+    if N == 999:
+        print('Finalizando...')
+        break
+    else:
+        if N < len(geral):
+            print(f'Notas de {geral[N][0]} são {geral[N][1]}')
+            print(' ')
+        else:
+            print('Número não encontrado!')
+            print(' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
