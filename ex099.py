ae1220f1d8698efeaec4d688f1c323a7941f1bf6
Lohana-Caruccio <lohana472@gmail.com>
2026-05-07 10:04:55 -0300

    Exercício novo

---
+++ ex099.py
@@ -0,0 +1,23 @@
+#Função que descobre o maior
+from time import sleep
+def maior (* num):
+    print('-'*50)
+    print(f'Analisando os valores passados...')
+    c = ma = 0
+    while c < len(num):
+        if c == 0:
+            ma = num[c]
+        elif num[c] > ma:
+            ma = num[c]
+        print(f'{num[c]} ', end=' ')
+        sleep(0.3)
+        c += 1
+    print(f'Foram informados {c} valores ao todo.')
+    print(f'O maior valor informado foi {ma}.')
+
+#Programa principal
+maior(2,9,4,5,7,1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
