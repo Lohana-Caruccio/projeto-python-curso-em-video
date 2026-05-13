892847977751c400a69c14a53c44ba1cabc6a1ef
Lohana-Caruccio <lohana472@gmail.com>
2026-05-13 15:46:08 -0300

    Exercício novo

---
+++ ex107/moeda/__init__.py
@@ -0,0 +1,16 @@
+#Funções
+def metade(num):
+    res = num / 2
+    return res
+
+def dobro(num):
+    res = num * 2
+    return res
+
+def aumentar(num, p):
+    res = num + (num * (p / 100))
+    return res
+
+def diminuir(num, p):
+    res = num - (num * (p /100))
+    return res
