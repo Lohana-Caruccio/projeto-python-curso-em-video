cd6883b7b4c3e4d528615f00c60620de568b5119
Lohana-Caruccio <lohana472@gmail.com>
2026-05-14 10:54:27 -0300

    Exercício novo

---
+++ ex110/moeda4/__init__.py
@@ -0,0 +1,45 @@
+#Funções Moeda2
+
+def metade(num=0 or 0.0, f=False):
+    res = num / 2
+    return res if f is False else moeda(res) #Forma usada pela resolução do Gustavo Guanabara
+
+def dobro(num=0 or 0.0, f=False):
+    res = num * 2
+    if f:
+        v = moeda(res)
+    else:
+        v = res
+    return v #Forma usada por mim para resolver
+
+def aumentar(num=0 or 0.0, p=0, f = False):
+    res = num + (num * (p / 100))
+    if f:
+        v = moeda(res)
+    else:
+        v = res
+    return v
+
+def diminuir(num=0 or 0.0, p=0, f = False):
+    res = num - (num * (p /100))
+    if f:
+        v = moeda(res)
+    else:
+        v = res
+    return v
+
+def moeda(num=0 or 0.0, m = 'R$'):
+    res = f'{m}{num:.2f}'.replace('.', ',') #Replace, troca todos os pontos por vírgulas
+    return res
+
+def resumo(num=0 or 0.0, a=10, d=5):
+    print('-'*30)
+    print('\033[36mRESUMO DO VALOR\033[m'.center(38)) #Colocou no centro
+    print('-'*30)
+    print(f'Preço Analisado: \t{moeda(num)}') #\t para alinhar
+    print(f'Dobro do preço: \t{dobro(num, True)}')
+    print(f'Metade do preço: \t{metade(num, True)}')
+    print(f'{a}% de aumento: \t{aumentar(num, a, True)}')
+    print(f'{d}% de redução:  \t{diminuir(num, d, True)}')
+    print('-'*30)
+
---
+++ ex110/teste.py
@@ -0,0 +1,7 @@
+#Exercitando módulos em Python com formatação chamando função moeda dentro das outras funções
+
+import moeda4
+
+p = float(input("Digite o preço do produto: R$"))
+moeda4.resumo(p,20,12)
+
